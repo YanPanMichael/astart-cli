f452ecc7ac5a74fdaeb76d6834987d46f844fc06
Michael <yanpancsu@icloud.com>
2022-05-16 21:27:25 +0800

    feat: add ts seed module

---
+++ packages/create/template-seed-ts-module/README.md
@@ -0,0 +1,11 @@
+# template-seed-ts-module
+
+> By.
+
+## 简介
+
+## 配置
+
+| 参数 | 类型 | default | 说明 |
+| --- | --- | --- | --- |
+
---
+++ packages/create/template-seed-ts-module/test/index.spec.ts
@@ -0,0 +1,7 @@
+import oar from '../dist/bundle.cjs.min.js'
+
+describe('基础功能测试.', () => {
+  it('功能1', () => {
+    expect(1).toEqual(1)
+  })
+})
